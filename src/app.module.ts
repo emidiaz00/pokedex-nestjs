8e703a277062a0db2ea7b58f9b5c409d40d01e11
emildsn <emildsn00@gmail.com>
2023-02-03 20:46:30 -0300

    Development (#2)
    * add serve-static
    
    * add resource pokemon
    
    ---------
    
    Co-authored-by: ediaz-wallypos <ediaz@wallypos.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,21 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { PokemonModule } from './pokemon/pokemon.module';
+
+
+
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..','public')
+
+    }),
+    PokemonModule
+
     })
+
   ],
 })
 export class AppModule {}
